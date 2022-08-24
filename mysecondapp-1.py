af97fe226f9450b2d61757ad721e809c037802bb
TLZ905 <111936584+TLZ905@users.noreply.github.com>
2022-08-24 11:17:56 +0800

    Add files via upload

---
+++ mysecondapp-1.py
@@ -0,0 +1,10 @@
+%%writefile mysecondapp.py
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.header("My second Streamlit App")
+st.write(pd.DataFrame({
+    'Intplan': ['yes', 'yes', 'yes', 'no'],
+    'Churn Status': [0, 0, 0, 1]
+}))
